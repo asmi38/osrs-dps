7ea58aa8e8df2e4707bee1a314cd727e5672ddb5
asmi38 <alecsmith@live.com>
2021-03-16 19:27:15 -0400

    changed default values
    changed default values

--- src/reducers/enemy.js
+++ src/reducers/enemy.js
@@ -1,7 +1,7 @@
 import { CHANGE_ENEMY } from '../actions/enemy'
 import MonsterData from '../data/monster-list'
 
-const default_monster = MonsterData["415"]
+const default_monster = MonsterData["8061"]
 
 export default function enemy (state = default_monster, action) {
   switch(action.type) {
