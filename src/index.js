fde0ecd02ff4549679d784c7eccc534d1f672760
asmi38 <alecsmith@live.com>
2021-03-22 21:53:24 -0400

    added antd
    added antd

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers/index.js'
 import middleware from './middleware'
+import 'antd/dist/antd.css';
+
 
 const store = createStore(reducer, middleware)
 
