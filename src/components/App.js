afa3994cd526ce12966a1ab8f6071bc72af37084
asmi38 <alecsmith@live.com>
2021-04-01 17:12:27 -0400

    added header, footer
    added header and footer to main app

--- src/components/App.js
+++ src/components/App.js
@@ -2,24 +2,38 @@ import React, { Component } from 'react';
 import Stats from './Stats'
 import Equipment from './Equipment'
 import Calcs from './Calcs'
-
 import Settings from './Settings'
+import { Layout } from 'antd'
+import GnomeChildIcon from '../data/icons/gnome-child.png'
+
+const { Header, Content, Footer, Sider } = Layout;
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-      <div className='container'>
-        <div className='settings-container'>
-          <Stats />
-          <Calcs />
-          <Settings />
-        </div>
-        <div className='equipment-container'>
-          <Equipment actionKey={"A"}/>
-          <Equipment actionKey={"B"}/>
-        </div>
-      </div>
+      <Layout>
+        <Header>
+          <div className='title-container'>
+            <div className='title-image'><img src={GnomeChildIcon} /></div>
+            <div className='title-text'> OSRS DPS</div>
+          </div>
+        </Header>
+        <Content>
+          <div className='container'>
+            <div className='settings-container'>
+              <Stats />
+              <Calcs />
+              <Settings />
+            </div>
+            <div className='equipment-container'>
+              <Equipment actionKey={"A"}/>
+              <Equipment actionKey={"B"}/>
+            </div>
+          </div>
+        </Content>
+        <Footer style={{textAlign: 'center' }}>OSRS DPS ©2021 Created by asmi38</Footer>
+      </Layout>
       </React.Fragment>
     )
   }
