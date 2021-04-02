88dd63299605627c8e251a00dab17c89f030893b
asmi38 <alecsmith@live.com>
2021-04-01 23:13:38 -0400

    removed table display
    removed table display

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -42,246 +42,161 @@ class Settings extends Component {
 
     return(
       <div className='settings'>
-        <Card size='small'>
+        <Card size='small' style={{width: 230}}>
         <Tabs defaultActiveKey="1">
-          <TabPane tab="Settings" key="1">
-            <table>
-              <tbody>
-              <tr>
-                <td>
-                  <img src={bgsIcon} alt="bandos godsword icon"/>
-                </td>
-                <td>
-                  <Tooltip placement="topLeft" title="Damage done by BGS specs" color={toolTipColour}>
-                      <span>BGS Specs</span>
-                  </Tooltip>
-                </td>
-                <td>
-                  <InputNumber
-                    value={calcs.bgs_dmg}
-                    style={{width: 60,}}
-                    onChange={(value) => this.handleChange("bgs_dmg", value)}
-                    min={0}
-                  />
-                </td>
-              </tr>
-
-              <tr>
-                <td>
-                  <img src={dwhIcon} alt="dragon warhammer icon"/>
-                </td>
-                <td>
-                  <Tooltip placement="topLeft" title="Number of DWH Special Attacks" color={toolTipColour}>
-                      <span>DWH Specials</span>
-                  </Tooltip>
-                </td>
-                <td>
-                  <InputNumber
-                    value={calcs.dwh_specials}
-                    style={{width: 60,}}
-                    onChange={(value) => this.handleChange("dwh_specials", value)}
-                    min={0}
-                  />
-                </td>
-              </tr>
 
 
-              <tr>
-                <td>
-                  <img src={arclightIcon} alt="arclight icon"/>
-                </td>
-                <td>
-                  <Tooltip placement="topLeft" title="Number of Arclight Special Attacks" color={toolTipColour}>
-                      <span>Arclight Specs</span>
-                  </Tooltip>
-                </td>
-                <td>
-                  <InputNumber
-                    value={calcs.arclight_specials}
-                    style={{width: 60,}}
-                    onChange={(value) => this.handleChange("arclight_specials", value)}
-                    min={0}
-                  />
-                </td>
-              </tr>
+            <TabPane tab="Settings" key="1">
+              <div className="options-row">
+                <img className='options-image' src={bgsIcon} alt="bandos godsword icon"/>
+                <Tooltip placement="topLeft" title="Damage done by BGS specs" color={toolTipColour}>
+                    <span className='options-name'>BGS Specs</span>
+                </Tooltip>
+                <InputNumber
+                  value={calcs.bgs_dmg}
+                  style={{width: 56, marginLeft: 'auto', alignSelf: 'center'}}
+                  onChange={(value) => this.handleChange("bgs_dmg", value)}
+                  min={0}
+                />
+              </div>
 
-              <tr>
-                <td>
-                  <img src={hpIcon} alt="hp icon"/>
-                </td>
-                <td>
-                  <Tooltip placement="topLeft" title="Remaining HP for Dharok's bonus" color={toolTipColour}>
-                      <span>Remaining HP</span>
-                  </Tooltip>
-                </td>
-                <td>
-                  <InputNumber
-                    value={calcs.remaining_hp}
-                    style={{width: 60,}}
-                    onChange={(value) => this.handleChange("remaining_hp", value)}
-                    min={1}
-                  />
-                </td>
-              </tr>
+              <div className="options-row">
+                <img className='options-image' src={dwhIcon} alt="dragon warhammer icon"/>
+                <Tooltip placement="topLeft" title="Number of DWH Special Attacks" color={toolTipColour}>
+                    <span className='options-name'>DWH Specials</span>
+                </Tooltip>
+                <InputNumber
+                  value={calcs.dwh_specials}
+                  style={{width: 56, marginLeft: 'auto', alignSelf: 'center'}}
+                  onChange={(value) => this.handleChange("dwh_specials", value)}
+                  min={0}
+                />
+              </div>
 
-              <tr>
-                <td>
-                  <img src={prayerIcon} alt="prayer icon"/>
-                </td>
-                <td>
-                  <Tooltip placement="topLeft" title="Remaining Prayer for Bludgeon Special" color={toolTipColour}>
-                      <span>Remaining Prayer</span>
-                  </Tooltip>
-                </td>
-                <td>
-                  <InputNumber
-                    value={calcs.remaining_prayer}
-                    style={{width: 60,}}
-                    onChange={(value) => this.handleChange("remaining_prayer", value)}
-                    min={0}
-                  />
-                </td>
-              </tr>
+              <div className="options-row">
+                <img className='options-image' src={arclightIcon} alt="arclight icon"/>
+                <Tooltip placement="topLeft" title="Number of Arclight Special Attacks" color={toolTipColour}>
+                      <span className='options-name'>Arclight Specs</span>
+                </Tooltip>
+                <InputNumber
+                  value={calcs.arclight_specials}
+                  style={{width: 56, marginLeft: 'auto', alignSelf: 'center'}}
+                  onChange={(value) => this.handleChange("arclight_specials", value)}
+                  min={0}
+                />
+              </div>
 
-              <tr>
-                <td>
-                  <img src={diaryIcon} alt="diary icon"/>
-                </td>
-                <td>
-                  <Tooltip placement="topLeft" title="Kandarin hard diary completed (10% increased bolt proc)" color={toolTipColour}>
-                      <span>Kandarin</span>
-                  </Tooltip>
-                </td>
-                <td>
-                  <Switch checked={calcs.kandarin_hard} onChange={(value) => this.handleChange("kandarin_hard", value)} />
-                </td>
-              </tr>
+              <div className="options-row">
+                <img className='options-image' src={hpIcon} alt="hp icon"/>
+                <Tooltip placement="topLeft" title="Remaining HP for Dharok's bonus" color={toolTipColour}>
+                    <span className='options-name'>Remaining HP</span>
+                </Tooltip>
+                <InputNumber
+                  value={calcs.remaining_hp}
+                  style={{width: 56, marginLeft: 'auto', alignSelf: 'center'}}
+                  onChange={(value) => this.handleChange("remaining_hp", value)}
+                  min={1}
+                />
+              </div>
 
-              <tr>
-                <td>
-                  <img src={slayerIcon} alt="slayer icon"/>
-                </td>
-                <td>
-                  <Tooltip placement="topLeft" title="Slayer task bonus if wearing Slayer Helm" color={toolTipColour}>
-                      <span>Slayer Task</span>
-                  </Tooltip>
-                </td>
-                <td>
-                  <Switch checked={calcs.slayer_task} onChange={(value) => this.handleChange("slayer_task", value)} />
-                </td>
-              </tr>
+              <div className="options-row">
+                <img className='options-image' src={prayerIcon} alt="prayer icon"/>
+                <Tooltip placement="topLeft" title="Remaining Prayer for Bludgeon Special" color={toolTipColour}>
+                    <span className='options-name'>Remaining Prayer</span>
+                </Tooltip>
+                <InputNumber
+                  value={calcs.remaining_prayer}
+                  style={{width: 56, marginLeft: 'auto', alignSelf: 'center'}}
+                  onChange={(value) => this.handleChange("remaining_prayer", value)}
+                  min={0}
+                />
+              </div>
 
-              <tr>
-                <td>
-                  <img src={skullIcon} alt="wilderness icon"/>
-                </td>
-                <td>
+              <div className="options-row">
+                <img className='options-image' src={diaryIcon} alt="diary icon"/>
+                <Tooltip placement="topLeft" title="Kandarin hard diary completed (10% increased bolt proc)" color={toolTipColour}>
+                    <span className='options-name'>Kandarin</span>
+                </Tooltip>
+                <Switch style={{marginLeft: 'auto', alignSelf: 'center'}} checked={calcs.kandarin_hard} onChange={(value) => this.handleChange("kandarin_hard", value)} />
+              </div>
+
+              <div className="options-row">
+                <img className='options-image' src={slayerIcon} alt="slayer icon"/>
+                <Tooltip placement="topLeft" title="Slayer task bonus if wearing Slayer Helm" color={toolTipColour}>
+                    <span className='options-name'>Slayer Task</span>
+                </Tooltip>
+                <Switch style={{marginLeft: 'auto', alignSelf: 'center'}} checked={calcs.slayer_task} onChange={(value) => this.handleChange("slayer_task", value)} />
+              </div>
+
+              <div className="options-row">
+                <img className='options-image' src={skullIcon} alt="wilderness icon"/>
                   <Tooltip placement="topLeft" title="If wilderness bonus applies, requires specific weapon for bonus" color={toolTipColour}>
-                      <span>Wilderness</span>
+                      <span className='options-name'>Wilderness</span>
                   </Tooltip>
-                </td>
-                <td>
-                  <Switch checked={calcs.wilderness} onChange={(value) => this.handleChange("wilderness", value)} />
-                </td>
-              </tr>
-              </tbody>
-            </table>
+                  <Switch style={{marginLeft: 'auto', alignSelf: 'center'}} checked={calcs.wilderness} onChange={(value) => this.handleChange("wilderness", value)} />
+              </div>
+            </TabPane>
+
+
+            <TabPane tab="League Relics" key="2">
+              <div className="options-container">
+                <div className="options-row">
+                  <b>Trailblazer</b>
+                </div>
+
+                <div className="options-row">
+                  <img className='options-image' src={quickShotIcon} alt="quick shot icon"/>
+                  <Popover placement="topLeft" content={quickShot} title="Quick Shot">
+                      <span className='options-name'>Quick shot</span>
+                  </Popover>
+                  <Switch style={{marginLeft: 'auto', alignSelf: 'center'}} checked={calcs.quick_shot} onChange={(value) => this.handleChange("quick_shot", value)} />
+                </div>
+
+                <div className="options-row">
+                  <img className='options-image' src={fluidStrikesIcon} alt="fluid strikes icon"/>
+                  <Popover placement="topLeft" content={fluidStrikes} title="Fluid Strikes">
+                      <span className='options-name'>Fluid Strikes</span>
+                  </Popover>
+                  <Switch style={{marginLeft: 'auto', alignSelf: 'center'}} checked={calcs.fluid_strikes} onChange={(value) => this.handleChange("fluid_strikes", value)} />
+                </div>
+
+                <div className="options-row">
+                  <img className='options-image' src={doubleCastIcon} alt="double cast icon"/>
+                  <Popover placement="topLeft" content={doubleCast} title="Double Cast">
+                      <span className='options-name'>Double Cast</span>
+                  </Popover>
+                    <Switch style={{marginLeft: 'auto', alignSelf: 'center'}} checked={calcs.double_cast} onChange={(value) => this.handleChange("double_cast", value)} />
+                </div>
+
+                <div className="options-row">
+                  <img className='options-image' src={tierSixIcon} alt="tier six icon"/>
+                  <Popover placement="topLeft" content={tierSix} title="Tier 6 Bonus">
+                      <span className='options-name'>Tier 6 bonus</span>
+                  </Popover>
+                    <Switch style={{marginLeft: 'auto', alignSelf: 'center'}} checked={calcs.tier6} onChange={(value) => this.handleChange("tier6", value)} />
+                </div>
+
+                <div className="options-row">
+                  <b>Twisted</b>
+                </div>
+
+                <div className="options-row">
+                  <img className='options-image' src={konarsBlessingIcon} alt="konars blessing icon"/>
+                  <Popover placement="topLeft" content={konarsBlessing} title="Konar's Blessing">
+                      <span className='options-name'>Konar's Blessing</span>
+                  </Popover>
+                  <Switch style={{marginLeft: 'auto', alignSelf: 'center'}} checked={calcs.konars_blessing} onChange={(value) => this.handleChange("konars_blessing", value)} />
+                </div>
+
+                <div className="options-row">
+                  <img className='options-image' src={xericsFocusIcon} alt="xerics focus icon"/>
+                  <Popover placement="topLeft" content={xericsFocus} title="Xeric's Focus">
+                      <span className='options-name'>Xeric's Focus</span>
+                  </Popover>
+                  <Switch style={{marginLeft: 'auto', alignSelf: 'center'}} checked={calcs.xerics_focus} onChange={(value) => this.handleChange("xerics_focus", value)} />
+                </div>
+              </div>
           </TabPane>
-
-
-          <TabPane tab="League Relics" key="2">
-          <table>
-            <tbody>
-            <tr><b>Trailblazer</b></tr>
-            <tr>
-              <td>
-                <img style={{width:"30px", height:"30px"}} src={quickShotIcon} alt="quick shot icon"/>
-              </td>
-              <td>
-                <Popover placement="topLeft" content={quickShot} title="Quick Shot">
-                    <span>Quick shot</span>
-                </Popover>
-              </td>
-              <td>
-                <Switch checked={calcs.quick_shot} onChange={(value) => this.handleChange("quick_shot", value)} />
-              </td>
-            </tr>
-
-            <tr>
-              <td>
-                <img src={fluidStrikesIcon} alt="fluid strikes icon"/>
-              </td>
-              <td>
-                <Popover placement="topLeft" content={fluidStrikes} title="Fluid Strikes">
-                    <span>Fluid Strikes</span>
-                </Popover>
-              </td>
-              <td>
-                <Switch checked={calcs.fluid_strikes} onChange={(value) => this.handleChange("fluid_strikes", value)} />
-              </td>
-            </tr>
-
-            <tr>
-              <td>
-                <img src={doubleCastIcon} alt="double cast icon"/>
-              </td>
-              <td>
-                <Popover placement="topLeft" content={doubleCast} title="Double Cast">
-                    <span>Double Cast</span>
-                </Popover>
-              </td>
-              <td>
-                  <Switch checked={calcs.double_cast} onChange={(value) => this.handleChange("double_cast", value)} />
-              </td>
-            </tr>
-
-            <tr>
-              <td>
-                <img src={tierSixIcon} alt="tier six icon"/>
-              </td>
-              <td>
-                <Popover placement="topLeft" content={tierSix} title="Tier 6 Bonus">
-                    <span>Tier 6 bonus</span>
-                </Popover>
-              </td>
-              <td>
-                  <Switch checked={calcs.tier6} onChange={(value) => this.handleChange("tier6", value)} />
-              </td>
-            </tr>
-
-            <tr><b>Twisted</b></tr>
-            <tr>
-              <td>
-                <img src={konarsBlessingIcon} alt="konars blessing icon"/>
-              </td>
-              <td>
-                <Popover placement="topLeft" content={konarsBlessing} title="Konar's Blessing">
-                    <span>Konar's Blessing</span>
-                </Popover>
-              </td>
-              <td>
-                <Switch checked={calcs.konars_blessing} onChange={(value) => this.handleChange("konars_blessing", value)} />
-              </td>
-            </tr>
-
-
-            <tr>
-              <td>
-                <img src={xericsFocusIcon} alt="xerics focus icon"/>
-              </td>
-              <td>
-                <Popover placement="topLeft" content={xericsFocus} title="Xeric's Focus">
-                    <span>Xeric's Focus</span>
-                </Popover>
-              </td>
-              <td>
-                <Switch checked={calcs.xerics_focus} onChange={(value) => this.handleChange("xerics_focus", value)} />
-              </td>
-            </tr>
-
-            </tbody>
-          </table>
-        </TabPane>
         </Tabs>
         </Card>
       </div>
